f1850395a15dd7e3077a69e3fb1a854a05fcf89f
Seth <seth@seths-mbp.mynetworksettings.com>
2023-12-19 17:56:04 -0500

    [feat/game-controller] - add baseline player obj + controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput.Map.Enable();
+        PlayerInput();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PlayerInput() 
+    {
+        playerInput.Map.Drop.started += OnDropPressed;
+    }
+
+    private void OnDropPressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Drop pressed");
+    }
+}
