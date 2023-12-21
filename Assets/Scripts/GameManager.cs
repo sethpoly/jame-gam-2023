63d3ac50fdb5c4f0827ebb3990cb0cb3c328956d
Seth <seth@seths-mbp.mynetworksettings.com>
2023-12-21 17:14:40 -0500

    [feat/difficulty-ranges] - add difficulty ranges

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,10 +15,10 @@ public class GameManager : MonoBehaviour
     private static readonly List<Difficulty> difficulties = new()
     {
         new Difficulty(conveyorSpeed: 5, minItemSpawnTime: .5f, maxItemSpawnTime: 2.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
-        new Difficulty(conveyorSpeed: 6, minItemSpawnTime: .5f, maxItemSpawnTime: 2.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
-        new Difficulty(conveyorSpeed: 7, minItemSpawnTime: .5f, maxItemSpawnTime: 2.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
-        new Difficulty(conveyorSpeed: 8, minItemSpawnTime: .5f, maxItemSpawnTime: 2.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
-        new Difficulty(conveyorSpeed: 9, minItemSpawnTime: .1f, maxItemSpawnTime: .5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero)
+        new Difficulty(conveyorSpeed: 6, minItemSpawnTime: .4f, maxItemSpawnTime: 2.0f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
+        new Difficulty(conveyorSpeed: 7, minItemSpawnTime: .3f, maxItemSpawnTime: 1.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
+        new Difficulty(conveyorSpeed: 8, minItemSpawnTime: .2f, maxItemSpawnTime: 1.5f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero),
+        new Difficulty(conveyorSpeed: 9, minItemSpawnTime: .15f, maxItemSpawnTime: 1f, levelTimer: 15f, anvilSpawnerLocation: Vector2.zero)
     };
 
     public DifficultyLevel difficultyLevel;
