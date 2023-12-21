29631aa1d478b4fa7ab7f221b554189e9cb780bf
Seth <seth@seths-mbp.mynetworksettings.com>
2023-12-21 18:10:04 -0500

    [feat/sign-logic] - make anvil drop straight

--- Assets/Scripts/Crushable.cs
+++ Assets/Scripts/Crushable.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class Crushable : MonoBehaviour
 {
     /// <summary>
-    /// Sent when an incoming collider makes contact with this object's
-    /// collider (2D physics only).
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
     /// </summary>
-    /// <param name="other">The Collision2D data associated with this collision.</param>
-    void OnCollisionEnter2D(Collision2D other)
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Collison with " + other.collider.tag);
-        if (!other.collider.CompareTag("Anvil")) {
+        Debug.Log("Collison with " + other.attachedRigidbody.tag);
+        if (!other.attachedRigidbody.CompareTag("Anvil")) {
             return;
         }
         Destroy(gameObject);
+        
     }
 }   
