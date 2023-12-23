4fd4da82468ce35ddcd1aa6b174d79e86e688983
Seth <seth@seths-mbp.mynetworksettings.com>
2023-12-22 20:46:50 -0500

    [feat/art-import] - add explosion animation

--- Assets/Scripts/Crushable.cs
+++ Assets/Scripts/Crushable.cs
@@ -1,8 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class Crushable : GameManagerObservable
 {
     public ItemType type;
+    public GameObject explosionPrefab;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private Rigidbody2D rb;
+
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -11,6 +18,10 @@ public class Crushable : GameManagerObservable
     void Start()
     {
         Initialize();
+        animator = explosionPrefab.GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     /// <summary>
@@ -32,8 +43,18 @@ public class Crushable : GameManagerObservable
         {
             gameManager.IncrementIncorrectCrushedItemsCount();
         }
+        StartCoroutine(Destroy());
+    }
 
+    IEnumerator Destroy()
+    {
+        // Explosion animation
+        spriteRenderer.enabled = false; // Hide this gameObject
+        rb.isKinematic = true;
+        boxCollider2D.enabled = false; // Disable collider
+        Instantiate(explosionPrefab, transform);
+        animator.Play("Explosion");
+        yield return new WaitForSecondsRealtime(1);
         Destroy(gameObject);
-        
     }
 }   
