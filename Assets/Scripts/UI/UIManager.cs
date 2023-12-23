b1509a18b240842ba114ea42430d2d3567220bd9
Seth <seth@seths-mbp.mynetworksettings.com>
2023-12-22 23:28:24 -0500

    [develop] - add level/coal crush count UI

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : GameManagerObservable
+{
+    public GameObject levelTextObj;
+    public GameObject coalCountTextObj;
+
+    private TextMeshProUGUI levelText;
+    private TextMeshProUGUI coalCountText;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Initialize();
+        levelText = levelTextObj.GetComponent<TextMeshProUGUI>();
+        coalCountText = coalCountTextObj.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update level number
+        if(levelText != null)
+        {
+            var levelNumber = (int)gameManager._currentLevel + 1;
+            levelText.SetText("Level " + levelNumber);
+        }
+
+        // Update coal count
+        if(coalCountText != null)
+        {
+            coalCountText.SetText(gameManager.crushedItemsCount + "/" + gameManager.currentLevel.itemsToCrush);
+        }
+    }
+}
